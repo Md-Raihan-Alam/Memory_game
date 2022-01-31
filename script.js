a23115b06ace9c377b48d1eb07663b28d16ce2ec
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-01-31 21:50:53 +0600

    W.I.P
    modal function is completed

---
+++ script.js
@@ -0,0 +1,5 @@
+const modal=document.querySelector('.modal_information');
+const btn=document.querySelector('.Close_btn');
+btn.addEventListener('click',()=>{
+    modal.classList.add('remove');
+});
