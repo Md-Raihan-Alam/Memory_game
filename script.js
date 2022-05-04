f0df79020c3ab6e1a2fd1cc779850563f640ca7b
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-05-04 17:44:01 +0600

    Code

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+const allPictures=[
+    {
+        id:1,
+        src:"Assets/cheseburger.jpg",
+        name:"Cheseburger",
+    },
+    {
+        id:2,
+        src:"Assets/fries.jpg",
+        name:"Fries",
+    },
+    {
+        id:3,
+        src:"Assets/hotdog.png",
+        name:"Hotdog",
+    },
+    {
+        id:4,
+        src:"Assets/ice-cream.jpg",
+        name:"Ice-cream",
+    },
+    {
+        id:5,
+        src:"Assets/milkshake.jpg",
+        name:"Milkshake",
+    },
+    {
+        id:6,
+        src:"Assets/pizza.png",
+        name:"Pizza",
+    },
+    {
+        id:7,
+        src:"Assets/cheseburger.jpg",
+        name:"Cheseburger",
+    },
+    {
+        id:8,
+        src:"Assets/fries.jpg",
+        name:"Fries",
+    },
+    {
+        id:9,
+        src:"Assets/hotdog.png",
+        name:"Hotdog",
+    },
+    {
+        id:10,
+        src:"Assets/ice-cream.jpg",
+        name:"Ice-cream",
+    },
+    {
+        id:11,
+        src:"Assets/milkshake.jpg",
+        name:"Milkshake",
+    },
+    {
+        id:12,
+        src:"Assets/pizza.png",
+        name:"Pizza",
+    }
+];
 const modal=document.querySelector('.modal_information');
 const btn=document.querySelector('.Close_btn');
 const gameMenu=document.querySelector('.game_menu');
@@ -5,12 +67,18 @@ const gameSetModal=document.querySelector(".game_menu_settings");
 const gameSettings=document.querySelector('.game_sett');
 const gameSettingsSubmit=document.querySelector('.submit');
 const gameStart=document.querySelector('.gameStart');
+const gameMode=document.querySelector('.game_hajimare');
+const gameResult=document.querySelector('.game_result');
 let gameLevel;
-document.querySelector('.hard').checked=true;
-btn.addEventListener('click',()=>{
-    modal.classList.add('remove');
-    gameMenu.classList.remove('remove');
-});
+let rememberSecond,matchingPoints,wrongMatchingPoints,timeMinus;
+let finalScore=0;
+let totalCards=12;
+let pictureChosen=[];
+let pictureParentId=[];
+let startTime;
+let finishTime;
+let seconds;
+document.querySelector('.normal').checked=true;
 gameSettings.addEventListener('click',()=>{
     gameSetModal.classList.remove('remove');
     gameMenu.classList.add('remove');
@@ -34,5 +102,128 @@ document.querySelector('.hardMode').addEventListener("click",()=>{
     gameLevel=document.querySelector('.hardMode').previousSibling.value;
 });
 gameStart.addEventListener('click',()=>{
-    console.log(gameLevel);
+    modal.classList.remove('remove');
+    gameMenu.classList.add('remove');
+    gameRules(gameLevel);
 });
+function shuffleArray(array){
+    for(let i=array.length-1;i>0;i--){
+        let randomArray=Math.floor(Math.random()*(i+1));
+        let temp=array[i];
+        array[i]=array[randomArray];
+        array[randomArray]=temp;
+    }
+}
+function gameFinishingAndSave(){
+    finishTime=performance.now();
+    let TimeDifference=finishTime-startTime;
+    seconds=Math.floor(TimeDifference/1000);
+    finalScore=finalScore+(seconds*timeMinus);
+    finalScore=finalScore.toFixed(2);
+    gameMode.classList.add('remove');
+    gameResult.classList.remove('remove');
+    document.querySelector('.player_result').innerHTML+=`
+        <div class="totalScore">Total Score:${finalScore}</div>
+        <div class="totalTime">Total Time:${seconds} seconds</div>
+        
+    `;
+    document.querySelector('.refresh_btn').addEventListener('click',()=>location.reload());
+}
+function calculateUserPoints(match){
+    if(match===true){
+        finalScore+=matchingPoints;
+        totalCards-=2;
+    }else{
+        finalScore+=wrongMatchingPoints;
+    }
+    if(totalCards===0) gameFinishingAndSave();
+    
+}
+function gameRules(levelDesc="normal"){
+    if(levelDesc==="easy"){
+        rememberSecond=1;
+        matchingPoints=3;
+        wrongMatchingPoints=-0.5;
+        timeMinus=-0.001;
+    }else if(levelDesc==="normal"){
+        rememberSecond=0.5;
+        matchingPoints=2;
+        wrongMatchingPoints=-1;
+        timeMinus=-0.01;
+    }else if(levelDesc==="hard"){
+        rememberSecond=0.1;
+        matchingPoints=1;
+        wrongMatchingPoints=-2;
+        timeMinus=-0.1;
+    }
+    let gameRulesDescription=` This is a memory game and you are in ${levelDesc} mode. You will have ${rememberSecond} second to memorize all images position and
+    then all picture will turn around. Now you will have to turn them around by clicking
+    on them. You have to match pair. If you match pairs you will get ${matchingPoints} points and  for a wrong
+    pair you will get ${wrongMatchingPoints}. The time will matter as well. With each time you will get ${timeMinus}.
+    The game will go on until you find all the pairs. After completing the game you will get to
+    save your score and name`
+    document.querySelector('.modal_information .desc').textContent=gameRulesDescription;
+    btn.addEventListener('click',()=>{
+        modal.classList.add('remove');
+        gameMenu.classList.add('remove');
+        gameMode.classList.remove('remove');
+        gameExecution(levelDesc);
+    });
+}
+function declareCurrentResult(){
+    document.querySelectorAll('.grid_items').forEach((e)=>e.parentElement.classList.remove('pointer_events_none'));
+    if(pictureChosen[0]===pictureChosen[1]){
+        calculateUserPoints(true);
+        for(let i of pictureParentId) document.querySelector(`[data-num="${i}"]`).children[1].classList.add("pointer_events_none");
+    }else{
+        calculateUserPoints(false);
+        for(let i of pictureParentId) document.querySelector(`[data-num="${i}"]`).classList.add("rotate");
+    }
+    pictureChosen=[];
+    pictureParentId=[];
+}
+function selectTimes(){
+    startTime=performance.now();
+  document.querySelectorAll('.grid_items').forEach((e)=>{
+        e.addEventListener('click',function(current){
+            if(pictureParentId.includes(current.target.parentElement.dataset.num)===false){
+                e.parentElement.classList.remove('rotate');
+                let currentImageDatasetId=current.target.dataset.id;
+                let currentDataSetId=current.target.parentElement.dataset.num;
+                pictureChosen.push(currentImageDatasetId);
+                pictureParentId.push(currentDataSetId);
+                if(pictureChosen.length===2) 
+                {
+                    document.querySelectorAll('.grid_items').forEach((e)=>e.parentElement.classList.add('pointer_events_none'));
+                    setTimeout(declareCurrentResult,1000);
+                }
+            }
+        });
+  });
+}
+function gameExecution(setLevel="hard"){
+    let gameTime;
+    if(setLevel==="easy"){
+        gameTime=1000;
+    }else if(setLevel==="normal"){
+        gameTime=500;
+    }else if(setLevel==="hard"){
+        gameTime=100;
+    }
+    shuffleArray(allPictures);
+    allPictures.forEach((e)=>{
+        document.querySelector('.game_sets').innerHTML+=`
+        <div class="transform_pic_3d" data-num="${e.id}">
+            <img src="Assets/blank.jpg" class="grid_items card_back"  data-id="${e.name}" alt="Blank"/>
+            <img src="${e.src}" class="grid_items card_front" alt="${e.name}" data-id="${e.name}"/>
+        </div>
+            `;
+    });
+    let allSelectedPictures=document.querySelectorAll('.transform_pic_3d');
+    setTimeout(()=>{
+        allSelectedPictures.forEach((e)=>{
+            e.classList.add('rotate');
+        });
+    },gameTime)
+    selectTimes();
+}
